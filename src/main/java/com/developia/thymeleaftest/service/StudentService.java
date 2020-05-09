8d6ea8060019faf2f680077068a243e98a27c33e
Shahriyar Novruzov <shahriyar.novruzov@pashabank.az>
2020-05-09 17:50:43 +0400

    first commit

---
+++ src/main/java/com/developia/thymeleaftest/service/StudentService.java
@@ -0,0 +1,31 @@
+package com.developia.thymeleaftest.service;
+
+import com.developia.thymeleaftest.model.Student;
+import com.developia.thymeleaftest.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class StudentService {
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    public void create(Student student) {
+        if (student.getFirstName().length() > 20) {
+            throw new IllegalArgumentException("too long name");
+        }
+
+        if (student.getPhone() != null && !student.getPhone().startsWith("+994")) {
+            throw new IllegalArgumentException("illegal phone number");
+        }
+
+        studentRepository.save(student);
+    }
+
+    public List<Student> findAll() {
+        return studentRepository.findAll();
+    }
+}
---
+++ src/main/java/com/developia/thymeleaftest/service/TestService.java
@@ -0,0 +1,27 @@
+package com.developia.thymeleaftest.service;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+@Profile("test")
+@Service
+public class TestService {
+
+    public void test(){
+
+        /*
+
+
+
+
+
+            some test logic
+
+
+
+
+
+         */
+    }
+
+}
